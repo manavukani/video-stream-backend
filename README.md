c8163c178baf8c7b48fbfda3a95bed08171b1186
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2024-03-01 13:31:16 +0530

    added utils

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Backend for Video Streaming
 
 #### Workflow
+
 1. Create database model ([Click Here](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj))
+2. Add devDependencies : `npm i -D prettier` & `npm i -D nodemon`
+3. Connect Database - MongoDB for this project
+   1. Wrap in Try-Catch
+   2. Use async
+4. Setup middlewares like cors, cookieParser, static - "public" folder, etc.
+5. Build "Utils" as required
+
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+// Using promise
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+/*
+const asyncHandler = () => {}
+const asyncHandler = (func) => {() => {}}
+const asyncHandler = (func) => async () => {}
+*/
+
+// Using async await
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export { asyncHandler };
